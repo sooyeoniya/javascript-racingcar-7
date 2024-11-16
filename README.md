76e2419bb9c4afb1bf329053a58eaa2f292e6f12
sooyeoniya <tndus1306403@naver.com>
2024-11-16 23:01:39 +0900

    test: validateCarNames 함수 테스트 구현

--- README.md
+++ README.md
@@ -47,5 +47,5 @@
 
   - [ ] 단위 테스트
     - [x] parser 함수 테스트
-    - [ ] validateCarNames 함수 테스트
+    - [x] validateCarNames 함수 테스트
     - [ ] validateTryCount 함수 테스트
---
+++ __tests__/ValidateCarNamesTest.js
@@ -0,0 +1,17 @@
+import validateCarNames from '../src/validations/validateCarNames.js';
+import { ERROR_PREFIX, ERROR_MESSAGES } from '../src/constants/constants.js';
+
+describe('validateCarNames 함수 테스트', () => {
+  it.each([
+    // given
+    ['validateIsEmpty: 자동차 이름이 비어있는지 확인', [ '' ], ERROR_MESSAGES.IS_EMPTY],
+    ['validateDuplicateName: 자동차 이름이 중복되는지 확인', [ 'pobi', 'woni', 'pobi' ], ERROR_MESSAGES.DUPLICATE_NAME],
+    ['validateHasBlankSpace: 자동차 이름 형식이 올바르지 않은 경우', [ 'po bi', 'won i' ], ERROR_MESSAGES.NAME_FORM],
+    ['validateNameLength: 자동차 이름이 5자를 초과하는 경우', [ 'pobi', 'javaji' ], ERROR_MESSAGES.NAME_LENGTH],
+  ])('%s', (_, input, errorMessage) => {
+    // when & then
+    expect(() => {
+      validateCarNames(input)
+    }).toThrow(`${ERROR_PREFIX} ${errorMessage}`);
+  });
+});
